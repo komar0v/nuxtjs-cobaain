e38b9bd9088f65a38fdcee725326e177bf0111d9
komar0v <fransiscodiaz81@gmail.com>
2020-11-19 13:16:07 +0700

    l

---
+++ pages/halamandua.vue
@@ -0,0 +1,63 @@
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">ini halaman dua</h1>
+      <h2 class="subtitle">{{pesan}}</h2>
+      <div class="links">
+        <a @click="showAlert" class="button--green">
+          Klik disini
+        </a>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      pesan: "yoii ini halaman 2 pake NUXT JS",
+    };
+  },
+  methods: {
+    showAlert(e) {
+      alert(this.pesan);
+    },
+  },
+};
+</script>
